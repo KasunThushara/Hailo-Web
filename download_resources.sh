6f9cbd4f722432a6147663d2e50b41ce8666ead2
kasun thushara <73091563+KasunThushara@users.noreply.github.com>
2025-04-07 12:32:10 +0530

    Update download_resources.sh

--- download_resources.sh
+++ download_resources.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
 wget https://ultralytics.com/images/zidane.jpg
-wget https://hailo-model-zoo.s3.eu-west-2.amazonaws.com/ModelZoo/Compiled/v2.14.0/hailo8l/yolov8n.hef
-wget https://hailo-model-zoo.s3.eu-west-2.amazonaws.com/ModelZoo/Compiled/v2.14.0/hailo8l/yolov8s_pose.hef
+wget https://hailo-model-zoo.s3.eu-west-2.amazonaws.com/ModelZoo/Compiled/v2.15.0/hailo8/yolov8n.hef
+wget https://hailo-model-zoo.s3.eu-west-2.amazonaws.com/ModelZoo/Compiled/v2.15.0/hailo8l/yolov8s_pose.hef
